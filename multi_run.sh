90537da9888ea91a6317eecd96d0998cbbd913eb
Wonhyung Shin <wonhyung64@gmail.com>
2024-03-31 16:14:11 +0900

    feat - experiment with batch variants

--- multi_run.sh
+++ multi_run.sh
@@ -2,20 +2,12 @@
 
 # python argparse source for experiments
 experiments=(
-# "--lr 1e-3 --weight-decay 1e-3 --batch-size 1024"
-# "--lr 1e-3 --weight-decay 1e-4 --batch-size 1024"
-# "--lr 1e-4 --weight-decay 1e-3 --batch-size 1024"
-# "--lr 1e-4 --weight-decay 1e-4 --batch-size 1024"
-# "--lr 1e-3 --weight-decay 1e-3 --batch-size 1024"
-"--lr 1e-3 --weight-decay 1e-4 --batch-size 1024"
-"--lr 1e-4 --weight-decay 1e-3 --batch-size 1024"
-"--lr 1e-4 --weight-decay 1e-4 --batch-size 1024"
-
-"--lr 5e-5 --weight-decay 1e-4 --batch-size 512"
-"--lr 1e-5 --weight-decay 1e-4 --batch-size 512"
-"--lr 1e-4 --weight-decay 1e-4 --batch-size 256"
-"--lr 5e-5 --weight-decay 1e-4 --batch-size 256"
-"--lr 1e-5 --weight-decay 1e-4 --batch-size 256"
+"--lr 1e-4 --weight-decay 1e-4 --batch-size 128"
+"--lr 5e-5 --weight-decay 1e-4 --batch-size 128"
+"--lr 1e-5 --weight-decay 1e-4 --batch-size 128"
+"--lr 1e-4 --weight-decay 1e-4 --batch-size 64"
+"--lr 5e-5 --weight-decay 1e-4 --batch-size 64"
+"--lr 1e-5 --weight-decay 1e-4 --batch-size 64"
 )
 
 # default prefix of job name
@@ -37,8 +29,8 @@ RUN_SRC=./run_src.sh
 ENV=/home1/wonhyung64/anaconda3/envs/openmmlab/bin/python3
 
 # file directory of experiment ".py"
-# EXECUTION_FILE=/home1/wonhyung64/Github/DRS/yahoo_implicit.py                   #OURS
-EXECUTION_FILE=/home1/wonhyung64/Github/DRS/baselines/ncf/yahoo_implicit.py   #NCF
+EXECUTION_FILE=/home1/wonhyung64/Github/DRS/yahoo_implicit.py                   #OURS
+# EXECUTION_FILE=/home1/wonhyung64/Github/DRS/baselines/ncf/yahoo_implicit.py   #NCF
 
 
 # data directory for experiments
@@ -48,6 +40,6 @@ DATA_DIR=/home1/wonhyung64/Github/DRS/data
 
 for index in ${!experiments[*]}; do
     # echo --job-name=$DEFAULT_NAME$index ${DEVICES[0]} $RUN_SRC $ENV $EXECUTION_FILE --data-dir $DATA_DIR ${experiments[$index]} 
-    sbatch --job-name=$DEFAULT_NAME$index ${DEVICES[2]} $RUN_SRC $ENV $EXECUTION_FILE --data-dir $DATA_DIR ${experiments[$index]} 
+    sbatch --job-name=$DEFAULT_NAME$index ${DEVICES[0]} $RUN_SRC $ENV $EXECUTION_FILE --data-dir $DATA_DIR ${experiments[$index]} 
     sleep 1
 done
