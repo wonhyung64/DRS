5bd5f9d5785b87675296071fefae28d65b718227
WonHyung Shin <wonhyung64@icloud.com>
2024-06-17 11:05:38 +0900

    feat - add coat and rename

--- baselines/ncf/train.py
+++ baselines/ncf/train.py
@@ -4,6 +4,7 @@ import torch
 import argparse
 import subprocess
 import numpy as np
+import pandas as pd
 from datetime import datetime
 
 from model import NCF
@@ -84,22 +85,32 @@ wandb.run.name = f"ncf_{expt_num}"
 
 #%% DATA LOADER
 data_set_dir = os.path.join(data_dir, dataset_name)
-train_file = os.path.join(data_set_dir, "ydata-ymusic-rating-study-v1_0-train.txt")
-test_file = os.path.join(data_set_dir, "ydata-ymusic-rating-study-v1_0-test.txt")
 
-x_train = []
-with open(train_file, "r") as f:
-    for line in f:
-        x_train.append(line.strip().split())
-# <user_id> <song id> <rating>
-x_train = np.array(x_train).astype(int)
+if dataset_name == "yahoo_r3":
+    train_file = os.path.join(data_set_dir, "ydata-ymusic-rating-study-v1_0-train.txt")
+    test_file = os.path.join(data_set_dir, "ydata-ymusic-rating-study-v1_0-test.txt")
 
-x_test = []
-with open(test_file, "r") as f:
-    for line in f:
-        x_test.append(line.strip().split())
-# <user_id> <song id> <rating>
-x_test = np.array(x_test).astype(int)
+    x_train = []
+    with open(train_file, "r") as f:
+        for line in f:
+            x_train.append(line.strip().split())
+    # <user_id> <song id> <rating>
+    x_train = np.array(x_train).astype(int)
+
+    x_test = []
+    with open(test_file, "r") as f:
+        for line in f:
+            x_test.append(line.strip().split())
+    # <user_id> <song id> <rating>
+    x_test = np.array(x_test).astype(int)
+
+
+elif dataset_name == "coat":
+    train_file = os.path.join(data_set_dir, "train.csv")
+    test_file = os.path.join(data_set_dir, "test.csv")
+    x_train = pd.read_csv(train_file).to_numpy()
+    x_test = pd.read_csv(test_file).to_numpy()
+
 
 print("===>Load from {} data set<===".format(dataset_name))
 print("[train] num data:", x_train.shape[0])
