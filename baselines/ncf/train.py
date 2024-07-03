ff311ba3ec15ddec202947d933846c736322af55
WonHyung Shin <wonhyung64@icloud.com>
2024-07-03 14:47:23 +0900

    feat - add kuairec

--- baselines/ncf/train.py
+++ baselines/ncf/train.py
@@ -106,10 +106,27 @@ if dataset_name == "yahoo_r3":
 elif dataset_name == "coat":
     train_file = os.path.join(data_set_dir, "train.csv")
     test_file = os.path.join(data_set_dir, "test.csv")
+
     x_train = pd.read_csv(train_file).to_numpy()
     x_train = np.stack([x_train[:,0]+1, x_train[:,1]+1, x_train[:,2]], axis=-1)
+
     x_test = pd.read_csv(test_file).to_numpy()
     x_test = np.stack([x_test[:,0]+1, x_test[:,1]+1, x_test[:,2]], axis=-1)
+
+elif dataset_name == "kuairec":
+    train_file = os.path.join(data_set_dir, "data/big_matrix.csv")
+    test_file = os.path.join(data_set_dir, "data/small_matrix.csv")
+
+    x_train = pd.read_csv(train_file)
+    x_train["interaction"] = x_train["watch_ratio"].map(lambda x: 1 if x >= 2. else 0)
+    x_train = x_train[["user_id", "video_id", "interaction"]].to_numpy()
+    x_train = np.stack([x_train[:,0]+1, x_train[:,1]+1, x_train[:,2]], axis=-1)
+
+    x_test = pd.read_csv(test_file)
+    x_test["interaction"] = x_test["watch_ratio"].map(lambda x: 1 if x >= 2. else 0)
+    x_test = x_test[["user_id", "video_id", "interaction"]].to_numpy()
+    x_test = np.stack([x_test[:,0]+1, x_test[:,1]+1, x_test[:,2]], axis=-1)
+    
 
 
 print("===>Load from {} data set<===".format(dataset_name))
@@ -119,8 +136,9 @@ print("[test]  num data:", x_test.shape[0])
 x_train, y_train = x_train[:,:-1], x_train[:,-1]
 x_test, y_test = x_test[:, :-1], x_test[:,-1]
 
-y_train = binarize(y_train)
-y_test = binarize(y_test)
+if dataset_name != "kuairec":
+    y_train = binarize(y_train)
+    y_test = binarize(y_test)
 
 num_sample = len(x_train)
 total_batch = num_sample // batch_size
