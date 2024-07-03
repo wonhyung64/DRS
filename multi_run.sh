ba15b528ea188d8a8a3a25f434fe617ceac6a489
Wonhyung Shin <wonhyung64@gmail.com>
2024-07-03 13:24:12 +0900

    feat - modify

--- multi_run.sh
+++ multi_run.sh
@@ -15,16 +15,21 @@ experiments=(
 # "--lr 1e-5 --weight-decay 1e-4 --embedding-k 64 --batch-size 64 --dataset-name yahoo_r3 --random-seed 4"
 
 #MF
-"--dataset-name coat --random-seed 0"
-"--dataset-name coat --random-seed 1"
-"--dataset-name coat --random-seed 2"
-"--dataset-name coat --random-seed 3"
-"--dataset-name coat --random-seed 4"
-"--dataset-name yahoo_r3 --random-seed 0"
-"--dataset-name yahoo_r3 --random-seed 1"
-"--dataset-name yahoo_r3 --random-seed 2"
-"--dataset-name yahoo_r3 --random-seed 3"
-"--dataset-name yahoo_r3 --random-seed 4"
+# "--dataset-name coat --random-seed 0"
+# "--dataset-name coat --random-seed 1"
+# "--dataset-name coat --random-seed 2"
+# "--dataset-name coat --random-seed 3"
+# "--dataset-name coat --random-seed 4"
+# "--dataset-name yahoo_r3 --random-seed 0"
+# "--dataset-name yahoo_r3 --random-seed 1"
+# "--dataset-name yahoo_r3 --random-seed 2"
+# "--dataset-name yahoo_r3 --random-seed 3"
+# "--dataset-name yahoo_r3 --random-seed 4"
+"--dataset-name kuairec --random-seed 0"
+"--dataset-name kuairec --random-seed 1"
+"--dataset-name kuairec --random-seed 2"
+"--dataset-name kuairec --random-seed 3"
+"--dataset-name kuairec --random-seed 4"
 
 # "--dataset-name yahoo_r3"
 )
@@ -38,6 +43,7 @@ DEVICES=(
     "--partition=gpu1 --gres=gpu:rtx3090:1 "
     "--partition=gpu2 --gres=gpu:a10:1 "
     "--partition=gpu4 --gres=gpu:a6000:1 "
+    "--partition=gpu5 --gres=gpu:a6000:1 "
     )
 
 # file directory of multiple execution source ".sh"
@@ -47,9 +53,9 @@ RUN_SRC=./run_src.sh
 ENV=/home1/wonhyung64/anaconda3/envs/openmmlab/bin/python3
 
 # file directory of experiment ".py"
-# EXECUTION_FILE=/home1/wonhyung64/Github/DRS/baselines/mf/train.py   #MF
+EXECUTION_FILE=/home1/wonhyung64/Github/DRS/baselines/mf/train.py   #MF
 # EXECUTION_FILE=/home1/wonhyung64/Github/DRS/baselines/mf_ips/train.py   #MF-IPS
-EXECUTION_FILE=/home1/wonhyung64/Github/DRS/baselines/mf_snips/train.py   #MF-SNIPS
+# EXECUTION_FILE=/home1/wonhyung64/Github/DRS/baselines/mf_snips/train.py   #MF-SNIPS
 
 # EXECUTION_FILE=/home1/wonhyung64/Github/DRS/baselines/ncf/train.py   #NCF
 # EXECUTION_FILE=/home1/wonhyung64/Github/DRS/baselines/ncf_ips/train.py   #NCF-IPS
