66f9b9d89a3678cd701e0dc9b1d60c45903c4df8
WonHyung Shin <wonhyung64@icloud.com>
2024-11-08 13:11:38 +0900

    feat - set simulation

--- simulation_case1.py
+++ simulation_case1.py
@@ -6,36 +6,12 @@ from sklearn.linear_model import LogisticRegression
 
 
 #%% 설정
-criteria_seed = 0
 repeat_num = 1000
 
 N = 1000000 # number of samples
 d = 3 # feature dimension
-theta_X_to_T = np.array([2.5, 1.0, -1.5, 0.5]) -3. # X -> T로 가는 경로의 계수
+theta_X_to_T = np.array([2.5, 1.0, -1.5, 0.5]) # X -> T로 가는 경로의 계수
 beta_XT_to_Y = np.array([2.2, 1.5, -2.0, 0.5, 2.0])  # X와 T -> Y로 가는 경로의 계수 (마지막은 T의 계수)
-
-#%% DATA GENERATION (Randomized data)
-# 1. 독립 변수 X 생성
-np.random.seed(criteria_seed)
-# x = np.random.normal(0, 1, (N, d))  # Normal(0,1)
-x = np.random.uniform(0, 1, (N, d))  # [0,1]  Uniform
-design_x = np.concatenate([np.ones([N,1]), x], axis=1)
-
-# 종속 변수 Y 생성 (X와 T의 영향을 받음)
-# Y는 X와 T의 선형 결합에 대한 로지스틱 확률로 결정되는 이진 변수
-dummy = np.ones(N).reshape(-1, 1)
-
-X_with_T = np.hstack((design_x, dummy))  
-logit_p_star = X_with_T.dot(beta_XT_to_Y)
-p_star = 1 / (1 + np.exp(-logit_p_star))
-np.random.seed(criteria_seed)
-y_forward = np.random.binomial(1, p_star)
-
-X_with_T_reverse = np.hstack((design_x, 1 - dummy))
-logit_p_star_reverse = X_with_T_reverse.dot(beta_XT_to_Y)
-p_star_reverse = 1 / (1 + np.exp(-logit_p_star_reverse))
-np.random.seed(criteria_seed)
-y_reverse = np.random.binomial(1, p_star_reverse)
 
 
 #%%
@@ -48,10 +24,30 @@ ipw_coef_list = []
 q_list = []
 
 for repeat_seed in tqdm(range(1, repeat_num+1)):
+    np.random.seed(repeat_seed)
+
+    # 1. 독립 변수 X 생성
+    x = np.random.normal(0, 1, (N, d))  # Normal(0,1)
+    # x = np.random.uniform(0, 1, (N, d))  # [0,1]  Uniform
+    design_x = np.concatenate([np.ones([N,1]), x], axis=1)
+
+    # 종속 변수 Y 생성 (X와 T의 영향을 받음)
+    # Y는 X와 T의 선형 결합에 대한 로지스틱 확률로 결정되는 이진 변수
+    dummy = np.ones(N).reshape(-1, 1)
+
+    X_with_T = np.hstack((design_x, dummy))  
+    logit_p_star = X_with_T.dot(beta_XT_to_Y)
+    p_star = 1 / (1 + np.exp(-logit_p_star))
+    y_forward = np.random.binomial(1, p_star)
+
+    X_with_T_reverse = np.hstack((design_x, 1 - dummy))
+    logit_p_star_reverse = X_with_T_reverse.dot(beta_XT_to_Y)
+    p_star_reverse = 1 / (1 + np.exp(-logit_p_star_reverse))
+    y_reverse = np.random.binomial(1, p_star_reverse)
+
 
     ##### randomized data generation ######
     q_star = np.ones(N) * 1/2
-    np.random.seed(repeat_seed)
     t_star = np.random.binomial(1, q_star)
     y_star = y_forward * t_star + y_reverse * (1-t_star)
 
@@ -63,17 +59,17 @@ for repeat_seed in tqdm(range(1, repeat_num+1)):
     x_treated = exposed_random_df[["X1", "X2", "X3"]].to_numpy()
     y_true = exposed_random_df["Y"].to_numpy()
 
+
     ##### randomized data fitting #####
-    np.random.seed(repeat_seed)
-    random_model = LogisticRegression()
-    random_model.fit(x_treated, y_true, sample_weight=np.ones_like(y_true)*2.)
+    random_model = LogisticRegression(C=10000)
+    random_model.fit(x_treated, y_true)
     random_bias_list.append(random_model.intercept_)
     random_coef_list.append(random_model.coef_)
+
 
     ##### partial data generation #####
     logit_q = design_x.dot(theta_X_to_T)
     q = 1 / (1 + np.exp(-logit_q))
-    np.random.seed(repeat_seed)
     t = np.random.binomial(1, q)
     y = y_forward * t + y_reverse * (1-t)
 
@@ -89,19 +85,20 @@ for repeat_seed in tqdm(range(1, repeat_num+1)):
     y_true = exposed_real_df["Y"].to_numpy()
     exposed_q = exposed_real_df["propensity"].to_numpy()
 
+
     ##### partial data fitting #####
-    np.random.seed(repeat_seed)
-    real_model = LogisticRegression()
+    real_model = LogisticRegression(C=10000)
     real_model.fit(x_treated, y_true)
     real_bias_list.append(real_model.intercept_)
     real_coef_list.append(real_model.coef_)
 
+
     ##### partial data ipw fitting #####
-    np.random.seed(repeat_seed)
-    ipw_model = LogisticRegression()
-    ipw_model.fit(x_treated, y_true, sample_weight=1/(2*exposed_q))
+    ipw_model = LogisticRegression(C=10000)
+    ipw_model.fit(x_treated, y_true, sample_weight=1/(exposed_q))
     ipw_bias_list.append(ipw_model.intercept_)
     ipw_coef_list.append(ipw_model.coef_)
+
 
 random_coef_arr = np.concatenate(random_coef_list, 0)
 real_coef_arr = np.concatenate(real_coef_list, 0)
--- simulation_case2.py
+++ simulation_case2.py
@@ -14,33 +14,6 @@ d = 3 # feature dimension
 theta_X_to_T = np.array([2.5, 1.0, -1.5, 0.5]) -50.  # X -> T로 가는 경로의 계수
 beta_XT_to_Y = np.array([2.2, 1.5, -2.0, 0.5, 2.0])  # X와 T -> Y로 가는 경로의 계수 (마지막은 T의 계수
 
-
-#%% DATA GENERATION (Randomized data)
-# 1. 독립 변수 X 생성
-np.random.seed(criteria_seed)
-x = np.random.normal(0, 1, (N, d))  # Normal(0,1)
-# x = np.random.uniform(0, 1, (N, d))  # [0,1]  Uniform
-design_x = np.concatenate([np.ones([N,1]), x], axis=1)
-
-
-# 3. 종속 변수 Y 생성 (X와 T의 영향을 받음)
-# Y는 X와 T의 선형 결합에 대한 로지스틱 확률로 결정되는 이진 변수
-dummy = np.ones(N).reshape(-1, 1)
-
-# Y(T=1) 생성
-X_with_T = np.hstack((design_x, dummy))
-logit_p_star = X_with_T.dot(beta_XT_to_Y)
-p_star = 1 / (1 + np.exp(-logit_p_star))
-np.random.seed(criteria_seed)
-y_forward = np.random.binomial(1, p_star)
-
-# Y(T=0) 생성
-X_with_T_reverse = np.hstack((design_x, 1 - dummy))
-logit_p_star_reverse = X_with_T_reverse.dot(beta_XT_to_Y)
-p_star_reverse = 1 / (1 + np.exp(-logit_p_star_reverse))
-np.random.seed(criteria_seed)
-y_reverse = np.random.binomial(1, p_star_reverse)
-
 true_cate = (1 / (1+np.exp(-beta_XT_to_Y[0] - beta_XT_to_Y[-1]))) - (1 / (1+np.exp(-beta_XT_to_Y[0])))
 
 
@@ -51,10 +24,32 @@ ipw_cate_list = []
 q_list = []
 
 for repeat_seed in tqdm(range(1, repeat_num+1)):
+    np.random.seed(repeat_seed)
+
+    # 독립 변수 X 생성
+    x = np.random.normal(0, 1, (N, d))  # Normal(0,1)
+    # x = np.random.uniform(0, 1, (N, d))  # [0,1]  Uniform
+    design_x = np.concatenate([np.ones([N,1]), x], axis=1)
+
+    # 종속 변수 Y 생성 (X와 T의 영향을 받음)
+    # Y는 X와 T의 선형 결합에 대한 로지스틱 확률로 결정되는 이진 변수
+    dummy = np.ones(N).reshape(-1, 1)
+
+    # Y(T=1) 생성
+    X_with_T = np.hstack((design_x, dummy))
+    logit_p_star = X_with_T.dot(beta_XT_to_Y)
+    p_star = 1 / (1 + np.exp(-logit_p_star))
+    y_forward = np.random.binomial(1, p_star)
+
+    # Y(T=0) 생성
+    X_with_T_reverse = np.hstack((design_x, 1 - dummy))
+    logit_p_star_reverse = X_with_T_reverse.dot(beta_XT_to_Y)
+    p_star_reverse = 1 / (1 + np.exp(-logit_p_star_reverse))
+    y_reverse = np.random.binomial(1, p_star_reverse)
+
 
     ##### randomized data generation #####
     q_star = np.ones(N) * 1/2
-    np.random.seed(repeat_seed)
     t_star = np.random.binomial(1, q_star)
     y_star = y_forward * t_star + y_reverse * (1-t_star)
 
@@ -74,7 +69,6 @@ for repeat_seed in tqdm(range(1, repeat_num+1)):
     ##### real data Y generation #####
     logit_q = design_x.dot(theta_X_to_T)
     q = 1 / (1 + np.exp(-logit_q))
-    np.random.seed(repeat_seed)
     t = np.random.binomial(1, q)
     y = y_forward * t + y_reverse * (1-t)
 
