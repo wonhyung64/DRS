8302fec22de65e6d9ef61c88957753059a3fee16
WonHyung Shin <wonhyung64@icloud.com>
2024-11-09 18:16:36 +0900

    feat - repetitions

--- simulation_case1.py
+++ simulation_case1.py
@@ -8,125 +8,138 @@ from sklearn.linear_model import LogisticRegression
 #%% 설정
 repeat_num = 1000
 
+# N = 10000 # number of samples
 N = 1000000 # number of samples
 d = 3 # feature dimension
-theta_X_to_T = np.array([2.5, 1.0, -1.5, 0.5]) -3.# X -> T로 가는 경로의 계수
-beta_XT_to_Y = np.array([2.2, 1.5, -2.0, 0.5, 2.0])  # X와 T -> Y로 가는 경로의 계수 (마지막은 T의 계수)
+theta_X_to_T_ = np.array([2.5, 1.0, -1.5, 0.5]) -3.# X -> T로 가는 경로의 계수
+# beta_XT_to_Y = np.array([2.2, 1.5, -2.0, 0.5, 2.0])  # X와 T -> Y로 가는 경로의 계수 (마지막은 T의 계수)
+beta_XT_to_Y = np.array([2.5, 1.0, -1.5, 0.5, 2.0])  # 
 
+# x_dist = "normal"
+x_dist = "uniform"
+
+# theta_loc_list = [0., 30., 50.] #normal 10^4
+theta_loc_list = [0., 0.32, 1.5] #uniform 10^4
+# theta_loc_list = [0., 0.3, 1.5, 3.] #uniform 10^6
 
 #%%
-random_bias_list = []
-real_bias_list = []
-ipw_bias_list = []
-random_coef_list = []
-real_coef_list = []
-ipw_coef_list = []
-q_list = []
+for theta_loc in theta_loc_list:
+    print(f"theta_loc : {theta_loc}")
 
-for repeat_seed in tqdm(range(1, repeat_num+1)):
-    np.random.seed(repeat_seed)
+    theta_X_to_T = theta_X_to_T_ - theta_loc
+    random_bias_list = []
+    real_bias_list = []
+    ipw_bias_list = []
+    random_coef_list = []
+    real_coef_list = []
+    ipw_coef_list = []
+    q_list = []
 
-    # 1. 독립 변수 X 생성
-    # x = np.random.normal(0, 1, (N, d))  # Normal(0,1)
-    x = np.random.uniform(0, 1, (N, d))  # [0,1]  Uniform
-    design_x = np.concatenate([np.ones([N,1]), x], axis=1)
+    for repeat_seed in tqdm(range(1, repeat_num+1)):
+        np.random.seed(repeat_seed)
 
-    # 종속 변수 Y 생성 (X와 T의 영향을 받음)
-    # Y는 X와 T의 선형 결합에 대한 로지스틱 확률로 결정되는 이진 변수
-    dummy = np.ones(N).reshape(-1, 1)
+        # 1. 독립 변수 X 생성
+        if x_dist == "normal":
+            x = np.random.normal(0, 1, (N, d))  # Normal(0,1)
+        elif x_dist == "uniform":
+            x = np.random.uniform(0, 1, (N, d))  # [0,1]  Uniform
+        design_x = np.concatenate([np.ones([N,1]), x], axis=1)
 
-    X_with_T = np.hstack((design_x, dummy))  
-    logit_p_star = X_with_T.dot(beta_XT_to_Y)
-    p_star = 1 / (1 + np.exp(-logit_p_star))
-    y_forward = np.random.binomial(1, p_star)
+        # 종속 변수 Y 생성 (X와 T의 영향을 받음)
+        # Y는 X와 T의 선형 결합에 대한 로지스틱 확률로 결정되는 이진 변수
+        dummy = np.ones(N).reshape(-1, 1)
 
-    X_with_T_reverse = np.hstack((design_x, 1 - dummy))
-    logit_p_star_reverse = X_with_T_reverse.dot(beta_XT_to_Y)
-    p_star_reverse = 1 / (1 + np.exp(-logit_p_star_reverse))
-    y_reverse = np.random.binomial(1, p_star_reverse)
+        X_with_T = np.hstack((design_x, dummy))  
+        logit_p_star = X_with_T.dot(beta_XT_to_Y)
+        p_star = 1 / (1 + np.exp(-logit_p_star))
+        y_forward = np.random.binomial(1, p_star)
 
-
-    ##### randomized data generation ######
-    q_star = np.ones(N) * 1/2
-    t_star = np.random.binomial(1, q_star)
-    y_star = y_forward * t_star + y_reverse * (1-t_star)
-
-    random_df = pd.DataFrame(x, columns=[f"X{i+1}" for i in range(d)])
-    random_df['T'] = t_star
-    random_df['Y'] = y_star
-
-    exposed_random_df = random_df[random_df["T"] == 1].reset_index(drop=True)
-    x_treated = exposed_random_df[["X1", "X2", "X3"]].to_numpy()
-    y_true = exposed_random_df["Y"].to_numpy()
+        X_with_T_reverse = np.hstack((design_x, 1 - dummy))
+        logit_p_star_reverse = X_with_T_reverse.dot(beta_XT_to_Y)
+        p_star_reverse = 1 / (1 + np.exp(-logit_p_star_reverse))
+        y_reverse = np.random.binomial(1, p_star_reverse)
 
 
-    ##### randomized data fitting #####
-    random_model = LogisticRegression(C=10000)
-    random_model.fit(x_treated, y_true)
-    random_bias_list.append(random_model.intercept_)
-    random_coef_list.append(random_model.coef_)
+        ##### randomized data generation ######
+        q_star = np.ones(N) * 1/2
+        t_star = np.random.binomial(1, q_star)
+        y_star = y_forward * t_star + y_reverse * (1-t_star)
+
+        random_df = pd.DataFrame(x, columns=[f"X{i+1}" for i in range(d)])
+        random_df['T'] = t_star
+        random_df['Y'] = y_star
+
+        exposed_random_df = random_df[random_df["T"] == 1].reset_index(drop=True)
+        x_treated = exposed_random_df[["X1", "X2", "X3"]].to_numpy()
+        y_true = exposed_random_df["Y"].to_numpy()
 
 
-    ##### partial data generation #####
-    logit_q = design_x.dot(theta_X_to_T)
-    q = 1 / (1 + np.exp(-logit_q))
-    t = np.random.binomial(1, q)
-    y = y_forward * t + y_reverse * (1-t)
-
-    q_list.append(q.mean())
-
-    real_df = pd.DataFrame(x, columns=[f"X{i+1}" for i in range(d)])
-    real_df['T'] = t
-    real_df['Y'] = y
-    real_df['propensity'] = q
-
-    exposed_real_df = real_df[real_df["T"] == 1].reset_index(drop=True)
-    x_treated = exposed_real_df[["X1", "X2", "X3"]].to_numpy()
-    y_true = exposed_real_df["Y"].to_numpy()
-    exposed_q = exposed_real_df["propensity"].to_numpy()
+        ##### randomized data fitting #####
+        random_model = LogisticRegression(C=10000)
+        random_model.fit(x_treated, y_true)
+        random_bias_list.append(random_model.intercept_)
+        random_coef_list.append(random_model.coef_)
 
 
-    ##### partial data fitting #####
-    real_model = LogisticRegression(C=10000)
-    real_model.fit(x_treated, y_true)
-    real_bias_list.append(real_model.intercept_)
-    real_coef_list.append(real_model.coef_)
+        ##### partial data generation #####
+        logit_q = design_x.dot(theta_X_to_T)
+        q = 1 / (1 + np.exp(-logit_q))
+        t = np.random.binomial(1, q)
+        y = y_forward * t + y_reverse * (1-t)
+
+        q_list.append(q.mean())
+
+        real_df = pd.DataFrame(x, columns=[f"X{i+1}" for i in range(d)])
+        real_df['T'] = t
+        real_df['Y'] = y
+        real_df['propensity'] = q
+
+        exposed_real_df = real_df[real_df["T"] == 1].reset_index(drop=True)
+        x_treated = exposed_real_df[["X1", "X2", "X3"]].to_numpy()
+        y_true = exposed_real_df["Y"].to_numpy()
+        exposed_q = exposed_real_df["propensity"].to_numpy()
 
 
-    ##### partial data ipw fitting #####
-    ipw_model = LogisticRegression(C=10000)
-    ipw_model.fit(x_treated, y_true, sample_weight=1/(exposed_q))
-    ipw_bias_list.append(ipw_model.intercept_)
-    ipw_coef_list.append(ipw_model.coef_)
+        ##### partial data fitting #####
+        real_model = LogisticRegression(C=10000)
+        real_model.fit(x_treated, y_true)
+        real_bias_list.append(real_model.intercept_)
+        real_coef_list.append(real_model.coef_)
 
 
-random_coef_arr = np.concatenate(random_coef_list, 0)
-real_coef_arr = np.concatenate(real_coef_list, 0)
-ipw_coef_arr = np.concatenate(ipw_coef_list, 0)
-
-random_bias_arr = np.concatenate(random_bias_list, 0)
-real_bias_arr = np.concatenate(real_bias_list, 0)
-ipw_bias_arr = np.concatenate(ipw_bias_list, 0)
+        ##### partial data ipw fitting #####
+        ipw_model = LogisticRegression(C=10000)
+        ipw_model.fit(x_treated, y_true, sample_weight=1/(exposed_q))
+        ipw_bias_list.append(ipw_model.intercept_)
+        ipw_coef_list.append(ipw_model.coef_)
 
 
-#%% BIAS
-true_beta = [beta_XT_to_Y[0] + beta_XT_to_Y[-1]] + list(beta_XT_to_Y[1:-1])
+    random_coef_arr = np.concatenate(random_coef_list, 0)
+    real_coef_arr = np.concatenate(real_coef_list, 0)
+    ipw_coef_arr = np.concatenate(ipw_coef_list, 0)
 
-print(f"q_bar : {np.mean(q_list).round(4)}\n")
+    random_bias_arr = np.concatenate(random_bias_list, 0)
+    real_bias_arr = np.concatenate(real_bias_list, 0)
+    ipw_bias_arr = np.concatenate(ipw_bias_list, 0)
 
-print(f"True Coef : \n{true_beta}\n")
+    true_beta = [beta_XT_to_Y[0] + beta_XT_to_Y[-1]] + list(beta_XT_to_Y[1:-1])
 
-print(f"""Random Coef\n{
-    [[(random_bias_arr - true_beta[0]).mean().round(4)]+[random_bias_arr.var().round(4)]] + 
-    [[(random_coef_arr[:,i] - true_beta[i+1]).mean().round(4), random_coef_arr[:,i].var().round(4)] for i in range(d)]
-}\n""")
 
-print(f"""Real Coef\n{
-    [[(real_bias_arr - true_beta[0]).mean().round(4)]+[real_bias_arr.var().round(4)]] + 
-    [[(real_coef_arr[:,i] - true_beta[i+1]).mean().round(4), real_coef_arr[:,i].var().round(4)] for i in range(d)]
-}\n""")
+    print(f"x_dist: {x_dist} / N={N} / theta_loc={theta_loc} / ")
+    print()
+    print(f"q_bar : {np.mean(q_list).round(4)}\n")
+    print(f"True Coef : \n{true_beta}\n")
+    print(f"""Random Coef\n{
+        [[(random_bias_arr - true_beta[0]).mean().round(4)]+[random_bias_arr.var().round(4)]] + 
+        [[(random_coef_arr[:,i] - true_beta[i+1]).mean().round(4), random_coef_arr[:,i].var().round(4)] for i in range(d)]
+    }\n""")
+    print(f"""Real Coef\n{
+        [[(real_bias_arr - true_beta[0]).mean().round(4)]+[real_bias_arr.var().round(4)]] + 
+        [[(real_coef_arr[:,i] - true_beta[i+1]).mean().round(4), real_coef_arr[:,i].var().round(4)] for i in range(d)]
+    }\n""")
+    print(f"""ipw Coef\n{
+        [[(ipw_bias_arr - true_beta[0]).mean().round(4)]+[ipw_bias_arr.var().round(4)]] + 
+        [[(ipw_coef_arr[:,i] - true_beta[i+1]).mean().round(4), ipw_coef_arr[:,i].var().round(4)] for i in range(d)]
+    }\n""")
 
-print(f"""ipw Coef\n{
-    [[(ipw_bias_arr - true_beta[0]).mean().round(4)]+[ipw_bias_arr.var().round(4)]] + 
-    [[(ipw_coef_arr[:,i] - true_beta[i+1]).mean().round(4), ipw_coef_arr[:,i].var().round(4)] for i in range(d)]
-}\n""")
+# %%
